77e8a52e38f7a0135a4d6b4a993263e359b7a2c3
Stephen Odundo <steveremdi@gmail.com>
2018-11-27 10:30:35 +0300

    Added DB code to Add assignments form

--- Schedule Manager/add_assignment.pas
+++ Schedule Manager/add_assignment.pas
@@ -5,7 +5,8 @@ unit add_assignment;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, sqlite3conn, sqldb, db, FileUtil, Forms, Controls,
+  Graphics, Dialogs, StdCtrls;
 
 type
 
@@ -14,13 +15,18 @@ type
   Tnew_add = class(TForm)
     Button1: TButton;
     Button2: TButton;
-    ComboBox1: TComboBox;
+    DataSource1: TDataSource;
     Edit1: TEdit;
+    Edit2: TEdit;
     GroupBox1: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
+    DBConnection: TSQLite3Connection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
 
   public
@@ -29,6 +35,7 @@ type
 
 var
   new_add: Tnew_add;
+  classname, assignment, classorder: string;
 
 implementation
 
@@ -38,12 +45,32 @@ implementation
 
 procedure Tnew_add.Button1Click(Sender: TObject);
 begin
-  Close;
+  assignment := Edit1.Text;
+  //classname := Edit2.Text;
+  classorder := '0';
+  DBConnection.Open;
+  SQLQuery1.Close;
+  //if SQLQuery1.Active = true then SQLQuery1.Active:= false;
+  SQLQuery1.SQL.Clear;
+  SQLQuery1.SQL.Add ('INSERT INTO assignment VALUES (:classnamep, :assignmentsp, :classorderp)');
+  DBConnection.Connected := True;
+  SQLTransaction1.Active := True;
+
+  SQLQuery1.Params.ParamByName('classnamep').AsString := Edit2.Text;
+  SQLQuery1.Params.ParamByName('assignmentsp').AsString := assignment;
+  SQLQuery1.Params.ParamByName('classorderp').AsString := classorder;
+  SQLQuery1.ExecSQL;
+  SQLTransaction1.Commit;
+  SQLQuery1.Open;
 end;
 
 procedure Tnew_add.Button2Click(Sender: TObject);
 begin
   Close;
+end;
+
+procedure Tnew_add.FormCreate(Sender: TObject);
+begin
 end;
 
 end.
